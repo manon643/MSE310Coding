32d33aba7d74c0205a2f9c05e4d43ff5f4ce0a73
SAPalmieri <stephen.palmieri1@gmail.com>
2019-11-21 16:56:28 -0800

    fixed rac - nnls
    fixed convergence

--- rac_mbadmm_nnls.m
+++ rac_mbadmm_nnls.m
@@ -6,14 +6,18 @@ blocks = 10;
 y = sprandn(n,1,.1);
 X = sprandn(n,p,.1);
 
+beta_true = pos(sprandn(p,1,.1));
+y = X*beta_true;
+
 beta0 = pos(sprandn(p,1,.1));
 z0 = pos(sprandn(p,1,.1));
 mu0 = pos(sprandn(p,1,.1));
 
 k = 1;
-err(k) = norm(beta0-z0,2);
-toler = 1e-4;
-maxIter = 500;
+err(k) = norm(beta_true-beta0,2);
+err_bz(k) = norm(beta0-z0,2);
+toler = 1e-3;
+maxIter = 1000;
 for ii = 1:maxIter
 
 [beta_out,z_out,mu_out] = nnls(y,X,beta0,z0, mu0, blocks);
@@ -23,9 +27,12 @@ mu0 = mu_out;
 beta(:,k) = beta_out;
 z(:,k) = z_out;
 mu(:,k) = mu_out;
+obj(k) = 1/(2*n) * (y-X*beta_out)'*(y-X*beta_out);
 k = k+1;
 
-err(k) = norm(beta_out-z_out,2);
+% err(k) = norm(beta_out-z_out,2);
+err(k) = norm(beta_true-beta_out,2);
+err_bz(k) = norm(beta_out-z_out,2);
 if err(k) <toler
     disp('below tolerance')
     break
@@ -38,17 +45,25 @@ figure
 plot(1:k,err)
 xlabel('iterations')
 ylabel('error')
-title('l-2 norm error')
+title('l-2 norm \beta error')
 
 figure
-plot(5:k,err(5:k))
+plot(1:k,err_bz)
 xlabel('iterations')
 ylabel('error')
-title('l-2 norm error excluding init')
+title('l-2 norm (\beta - z) error')
+
+figure
+plot(obj)
+xlabel('iterations')
+ylabel('objective loss')
+title('Non-negative Least Squares Objective Loss vs Iterations using RAC-MBADMM')
 
 function [beta, z, mu] = nnls(y, X, beta, z, mu, blocks)
-alpha = 1.8;
-gamma = 100;
+% alpha = 1.8; %this should be the same as the gamma according to their
+% paper
+
+gamma = 10;
 % gamma = 1000;
 [n,p] = size(X); 
 block_size = floor(p/blocks);
@@ -69,6 +84,6 @@ z = z(or);
     
     %max(z,0) is what pos does
     z = pos(-mu./(gamma) + beta);
-    mu = mu - alpha*(beta-z);
+    mu = mu - gamma*(beta-z);
     
 end
