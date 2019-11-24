1f9abbf257a5e84b8ec8634b5599b470e9c0fbce
SAPalmieri <stephen.palmieri1@gmail.com>
2019-11-24 17:20:28 -0500

    modular
    made functions modular and creating plotting/analyzing scripts

--- rac_mbadmm_nnls.m
+++ rac_mbadmm_nnls.m
@@ -6,73 +6,91 @@
 
 %% RAC
 clear all;close all;clc;
-n = 10;
-p = 20;
+% n = 10;
+% p = 20;
 n = 100;
 p = 200;
 blocks = 5;
 rng(5)
 y = sprandn(n,1,.1);
 X = sprandn(n,p,.1);
-
 beta_true = pos(sprandn(p,1,.1));
 y = X*beta_true;
-
 beta0 = pos(sprandn(p,1,.1));
 z0 = pos(sprandn(p,1,.1));
 mu0 = pos(sprandn(p,1,.1));
-gamma = 10;
-
 k = 1;
 err(k) = norm(beta_true-beta0,2);
 err_bz(k) = norm(beta0-z0,2);
 toler = 1e-4;
 maxIter = 1000;
-for ii = 1:maxIter
+gammas = [.1, 1, 10,1000];
 
-[beta_out,z_out,mu_out] = rac_nnls(y,X,beta0,z0, mu0, blocks);
-beta0 = beta_out;
-z0 = z_out;
-mu0 = mu_out;
-beta(:,k) = beta_out;
-z(:,k) = z_out;
-mu(:,k) = mu_out;
-obj(k) = 1/(2*n) * (y-X*beta_out)'*(y-X*beta_out);
-obj_al(k) = obj(k) + gamma/2*norm(beta_out-z_out,2)^2 - mu_out'*(beta_out-z_out);
-k = k+1;
-
-% err(k) = norm(beta_out-z_out,2);
-err(k) = norm(beta_true-beta_out,2);
-err_bz(k) = norm(beta_out-z_out,2);
-% if abs(err(k)-err(k-1))  < toler
-if err(k) < toler
-    disp('below tolerance')
-    break
-else
+for jj = 1:length(gammas)
+    for ii = 1:maxIter
+        
+        [beta_out,z_out,mu_out] = rac_nnls(y,X,beta0,z0, mu0, blocks,gammas(jj));
+        beta0 = beta_out; z0 = z_out;mu0 = mu_out;
+        %         beta(:,k) = beta_out;z(:,k) = z_out; mu(:,k) = mu_out;
+        
+        obj(k,jj) = 1/(2*n) * (y-X*beta_out)'*(y-X*beta_out);
+        obj_al(k,jj) = obj(k,jj) + gammas(jj)/2*norm(beta_out-z_out,2)^2 - mu_out'*(beta_out-z_out);
+        k = k+1;
+        
+        % err(k) = norm(beta_out-z_out,2);
+        err(k,jj) = norm(beta_true-beta_out,2);
+        err_bz(k,jj) = norm(beta_out-z_out,2);
+        % if abs(err(k)-err(k-1))  < toler
+        if err(k,jj) < toler
+            disp('below tolerance')
+            break
+        else
+        end
+        
+    end
+    k = 1;
 end
-
-end
+%verifying non-negativity constraints
+beta_out > 0
+z_out > 0
 
 figure
-plot(1:k,err)
+for iii = 1:length(gammas)
+    plot(err(:,iii))
+    hold on
+    
+end
 xlabel('iterations')
 ylabel('error')
 title('l-2 norm \beta error from true value')
+legend('.1','1','10','1000')
+hold off
 
 figure
-plot(1:k,err_bz)
+for iii = 1:length(gammas)
+    plot(err_bz(:,iii))
+    hold on
+    
+end
 xlabel('iterations')
 ylabel('error')
 title('l-2 norm (\beta - z) error')
+legend('.1','1','10','1000')
+hold off
 
 figure
-plot(obj)
-hold on
-% plot(obj_al)
-legend('original obj','augmented Lagrangian')
+for iii = 1:length(gammas)
+    plot(obj(:,iii))
+    hold on
+    %     plot(obj_al)
+    %     legend('original obj','augmented Lagrangian')
+    
+end
 xlabel('iterations')
 ylabel('objective loss')
 title('Non-negative Least Squares Objective Loss vs Iterations using RAC-MBADMM')
+legend('.1','1','10','1000')
+hold off
 
 %% Randomly Permute Comparison - Section IV
 clear all;close all;clc;
@@ -90,7 +108,7 @@ y = X*beta_true;
 beta0 = pos(sprandn(p,1,.1));
 z0 = pos(sprandn(p,1,.1));
 mu0 = pos(sprandn(p,1,.1));
-gamma = 10;
+gammas = [.1, 1, 10,1000];
 
 k = 1;
 err(k) = norm(beta_true-beta0,2);
@@ -98,163 +116,3 @@ err_bz(k) = norm(beta0-z0,2);
 toler = 1e-3;
 maxIter = 1000;
 
-for ii = 1:maxIter
-
-[beta_out,z_out,mu_out] = rp_nnls(y,X,beta0,z0, mu0, blocks);
-beta0 = beta_out;
-z0 = z_out;
-mu0 = mu_out;
-beta(:,k) = beta_out;
-z(:,k) = z_out;
-mu(:,k) = mu_out;
-obj(k) = 1/(2*n) * (y-X*beta_out)'*(y-X*beta_out);
-obj_al(k) = obj(k) + gamma/2*norm(beta_out-z_out,2)^2 - mu_out'*(beta_out-z_out);
-k = k+1;
-
-% err(k) = norm(beta_out-z_out,2);
-err(k) = norm(beta_true-beta_out,2);
-err_bz(k) = norm(beta_out-z_out,2);
-if err(k) <toler
-    disp('below tolerance')
-    break
-else
-end
-
-end
-figure
-plot(1:k,err)
-xlabel('iterations')
-ylabel('error')
-title('l-2 norm \beta error from true value')
-
-figure
-plot(1:k,err_bz)
-xlabel('iterations')
-ylabel('error')
-title('l-2 norm (\beta - z) error')
-
-figure
-plot(obj)
-hold on
-plot(obj_al)
-legend('original obj','augmented Lagrangian')
-xlabel('iterations')
-ylabel('objective loss')
-title('Non-negative Least Squares Objective Loss vs Iterations using RP-MBADMM')
-
-%% functions
-
-function [beta, z, mu] = rac_nnls(y, X, beta, z, mu, blocks)
-% alpha = 1.8; %this should be the same as the gamma according to their
-% paper
-
-gamma = 1;
-% gamma = 1000;
-[n,p] = size(X); 
-block_size = floor(p/blocks);
-or = randperm(2*p);
-
-% for each block
-    for j = 1:blocks
-        idx_lb = (j-1)*block_size +1;
-        idx_ub = idx_lb + block_size -1;
-        indices = or(idx_lb:idx_ub);
-        for ii = 1:block_size
-            val = indices(ii);
-            
-            if val < p %update beta
-                tmpX = X(:,indices(ii));
-                tmp(ii) = (1/n*tmpX'*tmpX + gamma) \ (1/n* tmpX'*y + mu(indices(ii)) + gamma*z(indices(ii)));
-            else %update z
-                if val == 2*p
-                    val_idx = val-p;
-                elseif val == p
-                    val_idx = val-p+1;
-                else
-                    val_idx = val-p+1;
-                end
-                tmp(ii) = pos(-mu(val_idx) / gamma + beta(val_idx));
-            end
-        end
-        for jj = 1:block_size
-            if indices(jj) < p
-                beta(indices(jj)) = tmp(jj);
-            else
-                if indices(jj) == 2*p
-                    val_idx = indices(jj)-p;
-                elseif indices(jj) == p
-                    val_idx = indices(jj)-p+1;
-                else
-                    val_idx = indices(jj)-p+1;
-                end
-                z(val_idx) = tmp(jj);
-            end
-        end
-        
-%         tmpX = X(:,indices);
-%         beta(indices) = -inv(1/n*tmpX'*tmpX + gamma*eye(block_size)) *(1/n* tmpX'*y - mu(indices) - gamma*z(indices));
-%         beta(indices) = (1/n*tmpX'*tmpX + gamma*eye(block_size)) \ (1/n* tmpX'*y + mu(indices) + gamma*z(indices));
-        
-    end
-    %max(z,0) is what pos does
-%     z = pos(-mu./(gamma) + beta);
-    mu = mu - gamma*(beta-z);
-    
-end
-
-function [beta, z, mu] = rp_nnls(y, X, beta, z, mu, blocks)
-% alpha = 1.8; %this should be the same as the gamma according to their
-% paper
-
-gamma = 1;
-[n,p] = size(X); 
-block_size = floor(p/blocks);
-or = randperm(2*p);
-
-% for each block
-    for j = 1:blocks
-        idx_lb = (j-1)*block_size +1;
-        idx_ub = idx_lb + block_size -1;
-        indices = or(idx_lb:idx_ub);
-        for ii = 1:block_size
-            val = indices(ii);
-            
-            if val < p %update beta
-                tmpX = X(:,indices(ii));
-                tmp(ii) = (1/n*tmpX'*tmpX + gamma) \ (1/n* tmpX'*y + mu(indices(ii)) + gamma*z(indices(ii)));
-            else %update z
-                if val == 2*p
-                    val_idx = val-p;
-                elseif val == p
-                    val_idx = val-p+1;
-                else
-                    val_idx = val-p+1;
-                end
-                tmp(ii) = pos(-mu(val_idx) / gamma + beta(val_idx));
-            end
-        end
-        for jj = 1:block_size
-            if indices(jj) < p
-                beta(indices(jj)) = tmp(jj);
-            else
-                if indices(jj) == 2*p
-                    val_idx = indices(jj)-p;
-                elseif indices(jj) == p
-                    val_idx = indices(jj)-p+1;
-                else
-                    val_idx = indices(jj)-p+1;
-                end
-                z(val_idx) = tmp(jj);
-            end
-        end
-        
-%         tmpX = X(:,indices);
-%         beta(indices) = -inv(1/n*tmpX'*tmpX + gamma*eye(block_size)) *(1/n* tmpX'*y - mu(indices) - gamma*z(indices));
-%         beta(indices) = (1/n*tmpX'*tmpX + gamma*eye(block_size)) \ (1/n* tmpX'*y + mu(indices) + gamma*z(indices));
-        
-    end
-    %max(z,0) is what pos does
-%     z = pos(-mu./(gamma) + beta);
-    mu = mu - gamma*(beta-z);
-    
-end
---
+++ rp_nnls.m
@@ -0,0 +1,52 @@
+function [beta, z, mu] = rp_nnls(y, X, beta, z, mu, blocks,gamma)
+[n,p] = size(X); 
+block_size = floor(p/blocks);
+or = randperm(2*p);
+
+% for each block
+    for j = 1:blocks
+        idx_lb = (j-1)*block_size +1;
+        idx_ub = idx_lb + block_size -1;
+        indices = or(idx_lb:idx_ub);
+        for ii = 1:block_size
+            val = indices(ii);
+            
+            if val < p %update beta
+                tmpX = X(:,indices(ii));
+                tmp(ii) = (1/n*tmpX'*tmpX + gamma) \ (1/n* tmpX'*y + mu(indices(ii)) + gamma*z(indices(ii)));
+            else %update z
+                if val == 2*p
+                    val_idx = val-p;
+                elseif val == p
+                    val_idx = val-p+1;
+                else
+                    val_idx = val-p+1;
+                end
+                tmp(ii) = pos(-mu(val_idx) / gamma + beta(val_idx));
+            end
+        end
+        for jj = 1:block_size
+            if indices(jj) < p
+                beta(indices(jj)) = tmp(jj);
+            else
+                if indices(jj) == 2*p
+                    val_idx = indices(jj)-p;
+                elseif indices(jj) == p
+                    val_idx = indices(jj)-p+1;
+                else
+                    val_idx = indices(jj)-p+1;
+                end
+                z(val_idx) = tmp(jj);
+            end
+        end
+        
+%         tmpX = X(:,indices);
+%         beta(indices) = -inv(1/n*tmpX'*tmpX + gamma*eye(block_size)) *(1/n* tmpX'*y - mu(indices) - gamma*z(indices));
+%         beta(indices) = (1/n*tmpX'*tmpX + gamma*eye(block_size)) \ (1/n* tmpX'*y + mu(indices) + gamma*z(indices));
+        
+    end
+    %max(z,0) is what pos does
+%     z = pos(-mu./(gamma) + beta);
+    mu = mu - gamma*(beta-z);
+    
+end
